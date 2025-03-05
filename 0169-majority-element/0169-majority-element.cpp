02f4d1ff88c5665592b64d7a8973d9ed0d32d59e
Aditya Pratap Singh <119702343+aditya762@users.noreply.github.com>
2025-03-05 14:41:25 +0530

    Time: 0 ms (100%), Space: 27.9 MB (97.88%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -3,8 +3,6 @@ public:
     int majorityElement(vector<int>& nums) {
        int n = nums.size();
        sort(nums.begin(), nums.end());
-       int a = n/2; 
-       int b = nums[a];
-       return b;
+       return nums[n/2];
     }
 };
