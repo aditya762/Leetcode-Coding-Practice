bcabf4294be1ad0d383d86c554c34c2d4b1c2768
Aditya Pratap Singh <119702343+aditya762@users.noreply.github.com>
2025-03-18 15:47:43 +0530

    Time: 0 ms (100%), Space: 7.9 MB (56.49%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,18 +1,15 @@
 class Solution {
 public:
     int climbStairs(int n) {
-        if (n <= 3) return n;
+        if (n <= 1) return 1;
 
-        int prev1 = 3;
-        int prev2 = 2;
-        int cur = 0;
-
-        for (int i = 3; i < n; i++) {
-            cur = prev1 + prev2;
+        int prev1 = 1, prev2 = 1, curr;
+        for (int i = 2; i <= n; i++) {
+            curr = prev1 + prev2;
             prev2 = prev1;
-            prev1 = cur;
+            prev1 = curr;
         }
 
-        return cur;        
+        return prev1;
     }
 };
