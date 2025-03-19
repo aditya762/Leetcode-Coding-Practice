4606e67bcc99e30c5a049902c7f5edcb98882462
Aditya Pratap Singh <119702343+aditya762@users.noreply.github.com>
2025-03-19 14:48:01 +0530

    Time: 1 ms (17.9%), Space: 12 MB (6.53%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -4,25 +4,28 @@ public:
         int m = obstacleGrid.size();
         int n = obstacleGrid[0].size();
         
-        // If the start or end point is an obstacle, no path is possible
-        if (obstacleGrid[0][0] == 1 || obstacleGrid[m-1][n-1] == 1) {
+        if (obstacleGrid[0][0] == 1 || obstacleGrid[m - 1][n - 1] == 1) {
             return 0;
         }
         
         vector<vector<int>> dp(m, vector<int>(n, 0));
-        dp[0][0] = 1; // Starting point
+        dp[0][0] = 1;
         
-        for (int i = 0; i < m; ++i) {
-            for (int j = 0; j < n; ++j) {
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
                 if (obstacleGrid[i][j] == 1) {
-                    dp[i][j] = 0; // No path through obstacles
+                    dp[i][j] = 0;
                 } else {
-                    if (i > 0) dp[i][j] += dp[i-1][j];
-                    if (j > 0) dp[i][j] += dp[i][j-1];
+                    if (i > 0) {
+                        dp[i][j] += dp[i - 1][j];
+                    }
+                    if (j > 0) {
+                        dp[i][j] += dp[i][j - 1];
+                    }
                 }
             }
         }
         
-        return dp[m-1][n-1];
+        return dp[m - 1][n - 1];
     }
 };
